70fd91d8e7cf51b238c68a5d8876ae7dea726cc4
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-10-29 10:47:08 +0545

    Compose multiple hoc together using recompose

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
-import React, { Component } from 'react';
-import { setDisplayName, setPropTypes } from 'recompose';
-import { connect } from 'redux';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose, setDisplayName, setPropTypes } from 'recompose';
 
-const User = ({ name, status }) =>
-    <div className="User">
+const { connect } = Redux();
+
+// We don't want this
+// nesting of everything
+// const User = connect()(setPropTypes({})(setDisplayName('User')(({ name, status }) =>
+//     <div className="User">
+//         { name }: { status }
+//     </div>
+// )));
+
+// Instead compose these hoc into one
+const enhance = compose(
+    setDisplayName('User'),
+    setPropTypes({
+        name: PropTypes.string.isRequired,
+        status: PropTypes.string
+    }),
+    connect()
+);
+
+const User = enhance(({ name, status, dispatch }) =>
+    <div
+        className="User"
+        onClick={() => dispatch({ type: 'USER_SELECTED' })}>
         { name }: { status }
     </div>
+);
+
+console.log(User.displayName);
 
 const App = () =>
     <div>
-        <User name="AJRedDevil" status="active"/>
+        <User name="AJRedDevil" status="active" />
     </div>
 
 export default App;
+
+
+// fake implementation of redux
+function Redux() {
+    return {
+        connect: () => (BaseComponent) => (props) =>
+            <BaseComponent
+                {...props}
+                dispatch={ ({ type }) => console.log(type + ' dispatched')}
+            />
+    }
+}
