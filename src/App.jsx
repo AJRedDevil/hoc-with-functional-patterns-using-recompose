323cbce8207863d2aa9f1db95adfcc65cd21a6f3
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-10-29 14:24:07 +0545

    Pre setup : 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,24 @@
 /*
 TITLE:
-Transform Props using Recompose
+Lock Props using Recompose
 
 DESCRIPTION:
-Learn how to use the 'mapProps' higher-order component to modify an
-existing component’s API (its props). 'mapProps' takes incoming props
-and changes them however you’d like; for example, filtering the props
-by a field.
+Learn how to use the ‘withProps’ higher order component to pre-fill a prop,
+unable to be overridden.
 */
 
 import React from 'react';
-import { mapProps } from 'recompose';
-
-let index = 0;
-const User = ({ name, status }) =>
-    <div className="User">{ name }—{ status }</div>
-
-const UserList = ({ users, status }) =>
-    <div className="UserList">
-    <h3>{ status} users</h3>
-        { users && users.map((user) => <User key={index++} {...user} />)}
-    </div>;
-
-
-const users = [
-    { name: "Tim", status: 'active' },
-    { name: "Bob", status: 'pending' },
-    { name: "Joe", status: 'active' },
-    { name: "Jim", status: 'inactive' },
-];
-
-const filterByStatus = (status) => mapProps(
-    ({ users }) => ({
-        status,
-        users: users.filter(u => u.status === status)
-    })
-);
-
-const ActiveUsers = filterByStatus('active')(UserList)
-const InactiveUsers = filterByStatus('inactive')(UserList)
-const PendingUsers = filterByStatus('pending')(UserList)
 
 const App = () =>
     <div className="App">
-        <ActiveUsers users={ users } />
-        <InactiveUsers users={ users } />
-        <PendingUsers users={ users } />
-    </div>
+        <header>
+            <a href='#/'>Logo</a>
+        </header>
+        <nav>
+            <a href='#/'>Home</a>
+            <a href='#/products'>Products</a>
+            <a href='#/checkout'>Checkout</a>
+        </nav>
+    </div>;
 
 export default App;
