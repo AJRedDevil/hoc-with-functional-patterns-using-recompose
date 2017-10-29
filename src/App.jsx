6c1eb43577fa4e2333e60f96760f6d1cb4701554
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-10-29 19:23:03 +0545

    Using componentFromProp

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,22 @@ depending on circumstance.
 */
 
 import React from 'react';
+import { compose, componentFromProp, withProps } from 'recompose';
+
+const Link = compose(
+    withProps(({ type='a', to='#' }) =>
+        type === 'a'
+            ? { type, href: to }
+            : { type, onClick(e) { window.location=to }}
+    )
+)(componentFromProp('type'));
 
 const App = () =>
     <div className="App">
         <a href='#/page1'>Anchor Link</a>
         <button onClick={ window.location='#/page2' }>Button Link</button>
+        <Link to='#/page1'>Anchor Link</Link>
+        <Link type='button' to='#/page2'>Button Link</Link>
     </div>
 
 export default App;
