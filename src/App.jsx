879158b664477d3e95f3c620e6fb1434ebf2f2b9
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-10-29 13:27:22 +0545

    Pre setup: 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,43 @@
 /*
 TITLE:
-Add Lifecycle Hooks to a Functional Stateless Component using Recompose
+Transform Props using Recompose
 
 DESCRIPTION:
-Learn how to use the 'lifecycle' higher-order component
-to conveniently use hooks without using a class component.
+Learn how to use the 'mapProps' higher-order component to modify an
+existing component’s API (its props). 'mapProps' takes incoming props
+and changes them however you’d like; for example, filtering the props
+by a field.
 */
 
 import React from 'react';
-import { lifecycle } from 'recompose';
 
-const configPromise = fetchConfiguration();
+let index = 0;
+const User = ({ name, status }) =>
+    <div className="User">{ name }—{ status }</div>
 
-const withConfig = lifecycle({
-    state: { config: {} },
-    componentDidMount() {
-        configPromise.then(config =>
-            this.setState({ config }));
-    }
-});
+const UserList = ({ users }) =>
+    <div className="UserList">
+        { users && users.map((user) => <User key={index++} {...user} />)}
+    </div>;
 
-const User = withConfig(({ name, status, config }) =>
-    <div className="User">
-        { name }
-        { config.showStatus && '—' + status }
-        { config.canDeleteUsers && <button>X</button> }
-    </div>
-);
+
+const users = [
+    { name: "Tim", status: 'active' },
+    { name: "Bob", status: 'active' },
+    { name: "Joe", status: 'active' },
+    { name: "Jim", status: 'inactive' },
+];
 
 const App = () =>
-    <div>
-        <User name="AJRedDevil" status="active" />
+    <div className="App">
+        <h3>active users</h3>
+        <UserList users={ users.filter(u => u.status === 'active')} />
+
+        <h3>inactive users</h3>
+        <UserList users={ users.filter(u => u.status === 'inactive')} />
+
+        <h3>pending users</h3>
+        <UserList users={ users.filter(u => u.status === 'pending')} />
     </div>
 
 export default App;
-
-
-// Mock Configuration
-
-const config = {
-    showStatus: true,
-    canDeleteUsers: true
-};
-
-function fetchConfiguration() {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(config), 300);
-    });
-}
