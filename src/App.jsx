de4372a28a4769d2a16ffbad36233ed4379688b9
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-10-29 11:29:54 +0545

    Custom HOC using withState and withHandlers

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,15 @@ components. No need for classes!
 import React from 'react';
 import { compose, withState, withHandlers } from 'recompose';
 
+const withToggle = compose(
+    withState('toggledOn', 'toggle', false),
+    withHandlers({
+        show: ({ toggle }) => (e) => toggle(true),
+        hide: ({ toggle }) => (e) => toggle(false),
+        toggle: ({ toggle }) => (e) => toggle((current) => !current)
+    })
+);
+
 const StatusList = () =>
     <div className="StatusList">
         <div>pending</div>
@@ -18,21 +27,19 @@ const StatusList = () =>
         <div>active</div>
     </div>
 
-const Status = withState('listShown', 'setListVisible', false)(
-    ({ status, listShown, setListVisible }) =>
-        <span onClick={ () => setListVisible((x) => !x) }>
+const Status = withToggle(({ status, toggledOn, toggle }) =>
+        <span onClick={ toggle }>
             { status }
-            { listShown && <StatusList /> }
+            { toggledOn && <StatusList /> }
         </span>
 );
 
-const Tooltip = withState('tooltipShown', 'setTooltipVisible', false)(
-    ({ text, children, tooltipShown, setTooltipVisible }) =>
+const Tooltip = withToggle(({ text, children, toggledOn, show, hide }) =>
         <span>
-            { tooltipShown && <div className="Tooltip">{ text }</div> }
+            { toggledOn && <div className="Tooltip">{ text }</div> }
             <span
-                onMouseEnter={() => setTooltipVisible(true) }
-                onMouseLeave={() => setTooltipVisible(false) }
+                onMouseEnter={ show }
+                onMouseLeave={ hide }
             >
                 { children }
             </span>
