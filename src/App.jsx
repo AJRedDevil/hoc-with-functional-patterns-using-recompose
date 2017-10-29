671dee7f213d9a8287dfe4882695089d07ca9ae2
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-10-29 10:07:26 +0545

    Writing hoc from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+const overrideProps = (overrideProps) => (BaseComponent) => (props) =>
+    <BaseComponent {...props} {...overrideProps} />;
+
+const alwaysBob = overrideProps({ name: 'Bob' });
+
+const neverRender = (BaseComponent) =>
+    class extends Component {
+        shouldComponentUpdate() {
+            return false;
+        }
+
+        render() {
+            return <BaseComponent {...this.props} />;
+        }
+    }
 
 const User = ({ name }) =>
     <div className="User">{ name }</div>
 
+const User2 = alwaysBob(User);
+const User3 = neverRender(User);
+
 const App = () =>
     <div>
         <User name="AJRedDevil" />
+        <User2 name="AJAzzurri" />
+        <User3 name="Steve" />
     </div>
 
 export default App;
