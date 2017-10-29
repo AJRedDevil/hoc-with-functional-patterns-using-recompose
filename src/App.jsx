8ff5d7de98317505df466c957357d7caeb49752e
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-10-29 19:02:41 +0545

    Pre-setup : 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,56 @@
 /*
 TITLE:
-Replace a Component with Non-Optimal States using Recompose
+Render Nothing in Place of a Component using Recompose
 
 DESCRIPTION:
-Learn how to use the ‘branch’ and ‘renderComponent’
-higher-order components to show errors or messaging when
-your component is in a non-optimal state. Avoid putting
-extraneous logic to show errors or messaging into your
-core component by organizing your non-optimal states into
-custom higher-order components.
+Learn how to use the ‘branch’ and ‘renderNothing’ higher-order
+components to render nothing when a certain prop condition is
+met. Sometimes you only want to render a component when valid
+props exist or are in a certain condition; ‘renderNothing’ is
+an easy way to completely remove the component when you don’t
+need to show it.
 */
 
 import React from 'react';
-import { compose, lifecycle, branch, renderComponent } from 'recompose';
 
 const User = ({ name, status}) =>
     <div className="User"> { name }-{ status } </div>;
 
-const withUserData = lifecycle({
-    componentDidMount() {
-        fetchData().then(
-            (users) => this.setState({ users }),
-            (error) => this.setState({ error })
-        );
-    }
-});
-
-const UNAUTHENTICATED = 401;
-const UNAUTHORIZED = 403;
-const errorMsgs = {
-    [UNAUTHENTICATED]: 'Not Authenticated!',
-    [UNAUTHORIZED]: 'Not Authorized!',
-};
-
-const AuthError = ({ error }) =>
-    error.statusCode &&
-        <div className="Error">{ errorMsgs[error.statusCode] }</div>;
-
-const NoUsersMessage = () =>
-    <div>There are no users to display</div>;
-
-const hasErrorCode = ({ error }) => error && error.statusCode;
-const hasNoUsers = ({ users }) => users && users.length === 0;
-
-const nonOptimalStates = (states) =>
-    compose(...states.map(state =>
-        branch(state.when, renderComponent(state.render))
-    ));
-
-const enhance = compose(
-    withUserData,
-    nonOptimalStates([
-        { when: hasErrorCode, render: AuthError },
-        { when: hasNoUsers, render: NoUsersMessage }
-    ])
-);
-
-const UserList = enhance(({ users }) =>
-    <div className="UserList">
-        { users && users.map((user) => <User {...user} />) }
-    </div>
-);
-
-const App = () =>
+const FeaturedUser = ({ name, status }) =>
     <div>
-        <UserList />
+        <h3>Today's Featured User</h3>
+        <User name={ name } status={ status } />
+        <hr />
     </div>;
 
-export default App;
+const UserList = ({ users }) =>
+    <div className="UserList">
+        <h3>All Users</h3>
+        { users && users.map((user) => <User key={user.name} {...user} />)}
+    </div>;
 
-
-// Mock Service
-const noUsers = [];
 const users = [
     { name: "Tim", status: "active" },
     { name: "Bob", status: "active" },
     { name: "Joe", status: "inactive" },
     { name: "Jim", status: "pending" },
 ];
-function fetchData() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            // reject({ statusCode: UNAUTHENTICATED });
-            // reject({ statusCode: UNAUTHORIZED })
-            resolve(noUsers);
-            // resolve(users);
-        }, 100);
-    });
+
+const featured = users[getRandomInt(0, 3)];
+
+const App = () =>
+    <div className="App">
+        <h2>User Management</h2>
+        <hr />
+        <FeaturedUser name={ featured.name } status={ featured.status } />
+        <UserList users= { users } />
+    </div>;
+
+export default App;
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
