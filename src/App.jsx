846b58af3d243300bdd592d9d1e3eefe4cc28024
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-10-29 19:17:42 +0545

    Pre-setup : 11

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,22 @@
 /*
 TITLE:
-Render Nothing in Place of a Component using Recompose
+Set the HTML Tag of a Component via a Prop using Recompose
 
 DESCRIPTION:
-Learn how to use the ‘branch’ and ‘renderNothing’ higher-order
-components to render nothing when a certain prop condition is
-met. Sometimes you only want to render a component when valid
-props exist or are in a certain condition; ‘renderNothing’ is
-an easy way to completely remove the component when you don’t
-need to show it.
+Learn how to user the ‘componentFromProp’ helper and ‘defaultProps’
+higher order component to swap the underlying html tag of your
+component. Sometimes we want a component to behave the same overall
+but to use a different element in the HTML output. An example is
+swapping an <a> for a <button> or even a react router <Link>
+depending on circumstance.
 */
 
 import React from 'react';
-import { compose, branch, renderNothing } from 'recompose';
-
-const User = ({ name, status}) =>
-    <div className="User"> { name }-{ status } </div>;
-
-const userIsNotActive = ({ status }) => status !== 'active';
-const hideIfNotActive = branch(userIsNotActive, renderNothing);
-
-const FeaturedUser = hideIfNotActive(({ name, status }) =>
-    <div>
-        <h3>Today's Featured User</h3>
-        <User name={ name } status={ status } />
-        <hr />
-    </div>
-);
-
-const UserList = ({ users }) =>
-    <div className="UserList">
-        <h3>All Users</h3>
-        { users && users.map((user) => <User key={user.name} {...user} />)}
-    </div>;
-
-const users = [
-    { name: "Tim", status: "active" },
-    { name: "Bob", status: "active" },
-    { name: "Joe", status: "inactive" },
-    { name: "Jim", status: "pending" },
-];
-
-const featured = users[getRandomInt(0, 3)];
 
 const App = () =>
     <div className="App">
-        <h2>User Management</h2>
-        <hr />
-        <FeaturedUser name={ featured.name } status={ featured.status } />
-        <UserList users= { users } />
-    </div>;
+        <a href='#/page1'>Anchor Link</a>
+        <button onClick={ window.location='#/page2' }>Button Link</button>
+    </div>
 
 export default App;
-
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
